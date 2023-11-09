31d35174016807d7b20c13837708d5e7db339fb0
Cedric B-Eff <cedrikopenclass@gmx.com>
2023-11-09 21:58:52 +0100

    cors issue to fix

--- src/App.tsx
+++ src/App.tsx
@@ -5,8 +5,8 @@ function App() {
 
   const formDefault = {
     baseIP: '127.0.0.1', 
-    port : 8080,
-    URIVar: '',
+    port : 9000,
+    URIVar: 'users',
     verb: '',
   }
 
@@ -15,27 +15,37 @@ function App() {
   function handleChange(event: React.FormEvent<HTMLInputElement | HTMLTextAreaElement | HTMLSelectElement>){
     const {name, value} = event.currentTarget
     setForm({...form, [name] : value})
-    console.log(value)
-}
+  }
+
+  async function handleSubmit(event: React.FormEvent<HTMLFormElement>){
+    event.preventDefault()
+    console.log('http://' + form.baseIP + ':' + form.port + '/' + form.URIVar)
+    // fetch('http://' + form.baseIP + ':' + form.port + '/' + form.URIVar).then(response => response.json()).then(json => console.log(json))
+    const response = await fetch('http://' + form.baseIP + ':' + form.port + '/' + form.URIVar);
+    const users = await response.json();
+    console.log(users)
+  }
 
   return (
     <main>
-
-      <label htmlFor='baseIP'>Base IP</label>
-      <input name="baseIP" onChange={handleChange} type="text" value={form.baseIP}/>
-      <label htmlFor="port">Port</label>
-      <input name="port" onChange={handleChange} type="text" value={form.port}/>
-      <label htmlFor="URIVar">URI + Vars</label>
-      <input name="URIVar" onChange={handleChange} type="text" value={form.URIVar}/>
-      <label>Method</label>
-      <select onChange={handleChange}>
-          <option value="get">Get</option>
-          <option value="post">Post</option>
-          <option value="update">Update</option>
-          <option value="delete">Delete</option>  
-      </select>
-      <label>Object</label>
-      <textarea/>
+      <form onSubmit={handleSubmit}>
+        <label htmlFor='baseIP'>Base IP</label>
+        <input name="baseIP" onChange={handleChange} type="text" value={form.baseIP}/>
+        <label htmlFor="port">Port</label>
+        <input name="port" onChange={handleChange} type="text" value={form.port}/>
+        <label htmlFor="URIVar">URI + Vars</label>
+        <input name="URIVar" onChange={handleChange} type="text" value={form.URIVar}/>
+        <label>Method</label>
+        <select onChange={handleChange}>
+            <option value="get">Get</option>
+            <option value="post">Post</option>
+            <option value="update">Update</option>
+            <option value="delete">Delete</option>  
+        </select>
+        <label>Object</label>
+        <textarea/>
+        <input type="submit" value="Send this Request"/>
+      </form>
     </main>
   )
 }
