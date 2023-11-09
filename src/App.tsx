31c7b88c683c7e35398aa524ca5dde526a2f207a
Cedric B-Eff <cedrikopenclass@gmx.com>
2023-11-09 21:14:24 +0100

    form state control

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,34 @@
+import { useState } from 'react'
 import './App.css'
 
 function App() {
 
+  const formDefault = {
+    baseIP: '127.0.0.1', 
+    port : 8080,
+    URIVar: '',
+    verb: '',
+  }
+
+  const [form, setForm] = useState<IForm>(formDefault)
+
+  function handleChange(event: React.FormEvent<HTMLInputElement | HTMLTextAreaElement | HTMLSelectElement>){
+    const {name, value} = event.currentTarget
+    setForm({...form, [name] : value})
+    console.log(value)
+}
+
   return (
     <main>
 
-      <label>Base IP</label>
-      <input type="text" value="127.0.0.1"/>
-      <label>Port</label>
-      <input type="text" value="8080"/>
-      <label>URI + Vars</label>
-      <input type="text" value="user"/>
+      <label htmlFor='baseIP'>Base IP</label>
+      <input name="baseIP" onChange={handleChange} type="text" value={form.baseIP}/>
+      <label htmlFor="port">Port</label>
+      <input name="port" onChange={handleChange} type="text" value={form.port}/>
+      <label htmlFor="URIVar">URI + Vars</label>
+      <input name="URIVar" onChange={handleChange} type="text" value={form.URIVar}/>
       <label>Method</label>
-      <select>
+      <select onChange={handleChange}>
           <option value="get">Get</option>
           <option value="post">Post</option>
           <option value="update">Update</option>
@@ -25,3 +41,10 @@ function App() {
 }
 
 export default App
+
+interface IForm{
+  baseIP: string
+  port: number
+  URIVar: string
+  verb: string
+}
