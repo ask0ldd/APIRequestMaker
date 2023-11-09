1a3b47597059224c7c794e3d5c85d81f129be556
Cedric B-Eff <cedrikopenclass@gmx.com>
2023-11-09 19:10:46 +0100

    form base

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,26 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <main>
+
+      <label>Base IP</label>
+      <input type="text" value="127.0.0.1"/>
+      <label>Port</label>
+      <input type="text" value="8080"/>
+      <label>URI + Vars</label>
+      <input type="text" value="user"/>
+      <label>Method</label>
+      <select>
+          <option value="get">Get</option>
+          <option value="post">Post</option>
+          <option value="update">Update</option>
+          <option value="delete">Delete</option>  
+      </select>
+      <label>Object</label>
+      <input type='textarea'/>
+    </main>
   )
 }
 
